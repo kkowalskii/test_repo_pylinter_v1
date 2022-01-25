4f2a36ffed2a774b9636314a87f48109e1aa9b3d
Kosma Kowalski <k.kowalski.655@studms.ug.edu.pl>
2022-01-25 15:33:35 +0100

    zmiany w main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-import math
 
-def func1(a, b) -> str:
-    return math.floor(a + b)
 
-def func2(a,b, c) -> int:
-    return os.getcwd()
+def add(a, b):
+    wynik = a + b
+    return wynik
+
+
